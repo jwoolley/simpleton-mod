0c8ea563fb5daff8e75b48ec405b4b7e8cfc2c86
Justin Woolley <19597672+jwoolley@users.noreply.github.com>
2019-05-22 18:55:07 -0700

    upgraded Cultivate now plants 2 crop stacks

--- src/main/java/thesimpleton/actions/CultivateAction.java
+++ src/main/java/thesimpleton/actions/CultivateAction.java
@@ -1,2 +1,91 @@
-public class CultivateAction {
+package thesimpleton.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.utility.SFXAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import org.apache.logging.log4j.Logger;
+import thesimpleton.TheSimpletonMod;
+import thesimpleton.powers.AbstractCropPower;
+import thesimpleton.powers.utils.Crop;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CultivateAction extends AbstractGameAction {
+  //TODO create "HARVEST/CROP" action type?
+  private static final ActionType ACTION_TYPE = ActionType.SPECIAL;
+  private static final float ACTION_DURATION = Settings.ACTION_DUR_LONG;
+
+  private AbstractPlayer p;
+  private final int numStacksToHarvest;
+  private final boolean isFromCard;
+
+  private boolean hasHarvested;
+  private boolean firstTickFinished;
+
+
+  private Map<Crop,Integer> cropCounts;
+
+  public CultivateAction(AbstractPlayer player, int numStacksToHarvest, int numStacksToGain, boolean isFromCard) {
+    this.p = player;
+    setValues(this.p,  this.p, numStacksToGain);
+    this.numStacksToHarvest = numStacksToHarvest;
+    this.amount = numStacksToGain;
+    this.isFromCard = isFromCard;
+
+    this.actionType = ACTION_TYPE;
+    this.duration = ACTION_DURATION;
+    this.firstTickFinished = false;
+    this.hasHarvested = false;
+
+    cropCounts = new HashMap();
+  }
+
+  public void update() {
+    Logger logger = TheSimpletonMod.logger;
+    if (this.duration != ACTION_DURATION) {
+      AbstractDungeon.actionManager.addToBottom(new WaitAction(0.1F));
+      logger.debug("CultivateAction.update :: second tick");
+      if (this.hasHarvested) {
+        for(Crop crop : cropCounts.keySet()) {
+          logger.debug("CultivateAction.update :: stacking " + crop + " for 1");
+          AbstractDungeon.actionManager.addToBottom(new ApplyCropAction(p, p, Crop.getCrop(crop, p, 1, this.isFromCard), 1, this.isFromCard));
+//
+//          AbstractDungeon.actionManager.addToBottom(
+//              new ApplyPowerAction(p, p,Crop.getCrop(crop, p, 1, this.isFromCard)));
+        }
+      }
+
+      this.tickDuration();
+      this.isDone = true;
+    }
+
+    if (!this.firstTickFinished) {
+      logger.debug("PruningAction.update :: first tick");
+
+      if (AbstractCropPower.playerHasAnyActiveCropPowers()) {
+        List<AbstractCropPower> crops = AbstractCropPower.getActiveCropPowers();
+
+        for(AbstractCropPower crop : crops) {
+          final int stacks = crop.amount < numStacksToHarvest ? crop.amount : numStacksToHarvest;
+          logger.debug("CultivateAction.update :: player has " + crop.amount + " stacks of " + crop.name);
+          logger.debug("CultivateAction.update :: harvesting " + numStacksToHarvest + " stacks of " + crop.name);
+          cropCounts.put(crop.enumValue, crop.amount);
+          crop.harvest(false, stacks);
+        }
+
+        this.hasHarvested = true;
+      } else {
+        AbstractDungeon.actionManager.addToBottom(new SFXAction("CARD_SELECT"));
+      }
+      this.firstTickFinished = true;
+    }
+
+    this.tickDuration();
+  }
 }
--- src/main/java/thesimpleton/cards/skill/Cultivate.java
+++ src/main/java/thesimpleton/cards/skill/Cultivate.java
@@ -1,5 +1,6 @@
 package thesimpleton.cards.skill;
 
+import basemod.abstracts.CustomCard;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -8,12 +9,17 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thesimpleton.TheSimpletonMod;
+import thesimpleton.actions.CultivateAction;
 import thesimpleton.cards.TheSimpletonCardTags;
+import thesimpleton.enums.AbstractCardEnum;
+import thesimpleton.powers.AbstractCropPower;
 
-public class Cultivate extends AbstractHarvestCard {
+public class Cultivate extends CustomCard {
   public static final String ID = "TheSimpletonMod:Cultivate";
   public static final String NAME;
   public static final String DESCRIPTION;
+  public static final String[] EXTENDED_DESCRIPTION;
+
   public static final String IMG_PATH = "cards/cultivate.png";
 
   private static final CardStrings cardStrings;
@@ -23,25 +29,28 @@ public class Cultivate extends AbstractHarvestCard {
   private static final AbstractCard.CardTarget TARGET = CardTarget.SELF;
 
   private static final int COST = 1;
-  private static final int UPGRADED_COST = 1;
   private static final int BLOCK = 6;
-  private static final int BLOCK_UPGRADE_BONUS = 2;
+  private static final int BLOCK_UPGRADE_BONUS = 3;
   private static final int HARVEST_AMOUNT = 3;
-  private static final int HARVEST_BONUS = 1;
-  private static final boolean HARVEST_ALL = false;
+  private static final int PLANT_AMOUNT = 1;
+  private static final int PLANT_AMOUNT_UPGRADE = 1;
+
 
   public Cultivate() {
-    super(ID, NAME, TheSimpletonMod.getResourcePath(IMG_PATH), COST, DESCRIPTION, TYPE, RARITY, TARGET,
-        HARVEST_AMOUNT, HARVEST_ALL, true);
+    super(ID, NAME, TheSimpletonMod.getResourcePath(IMG_PATH), COST, getDescription(), TYPE,
+        AbstractCardEnum.THE_SIMPLETON_BLUE, RARITY, TARGET);
+
     this.baseBlock = this.block = BLOCK;
     exhaust = true;
-    this.baseMagicNumber = this.magicNumber = HARVEST_AMOUNT;
+    this.baseMagicNumber = this.magicNumber = PLANT_AMOUNT;
     this.tags.add(TheSimpletonCardTags.HARVEST);
   }
 
   @Override
   public void use(AbstractPlayer p, AbstractMonster m) {
     AbstractDungeon.actionManager.addToBottom(new GainBlockAction(p, p, this.block));
+    AbstractDungeon.actionManager.addToBottom(
+        new CultivateAction(p, HARVEST_AMOUNT, this.magicNumber, true));
   }
 
   @Override
@@ -54,14 +63,18 @@ public class Cultivate extends AbstractHarvestCard {
     if (!this.upgraded) {
       upgradeName();
       upgradeBlock(BLOCK_UPGRADE_BONUS);
-      upgradeBaseCost(UPGRADED_COST);
-      upgradeMagicNumber(HARVEST_BONUS);
+      upgradeMagicNumber(PLANT_AMOUNT_UPGRADE);
     }
+  }
+
+  public static String getDescription() {
+    return DESCRIPTION + HARVEST_AMOUNT + EXTENDED_DESCRIPTION[0];
   }
 
   static {
     cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     NAME = cardStrings.NAME;
     DESCRIPTION = cardStrings.DESCRIPTION;
+    EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
   }
 }

491d43e113f397db9da4bc7d8bca6ef8486fd712
Justin Woolley <19597672+jwoolley@users.noreply.github.com>
2019-06-05 21:55:38 -0700

    fix chilis to not spawn asparagus instead

--- src/main/java/thesimpleton/crops/CropInfoDefinition.java
+++ src/main/java/thesimpleton/crops/CropInfoDefinition.java
@@ -19,55 +19,94 @@ abstract class CropInfoDefinition {
 
   public abstract AbstractCrop getCrop();
 
-  public static final CropInfoDefinition Artichokes;
-  public static final CropInfoDefinition Asparagus;
-  public static final CropInfoDefinition Chilis;
-  public static final CropInfoDefinition Corn;
-  public static final CropInfoDefinition Mushrooms;
-  public static final CropInfoDefinition Onions;
-  public static final CropInfoDefinition Potatoes;
-  public static final CropInfoDefinition Spinach;
-  public static final CropInfoDefinition Squash;
-  public static final CropInfoDefinition Turnips;
+  static final CropInfoDefinition Artichokes;
+  static final CropInfoDefinition Asparagus;
+  static final CropInfoDefinition Chilis;
+  static final CropInfoDefinition Corn;
+  static final CropInfoDefinition Mushrooms;
+  static final CropInfoDefinition Onions;
+  static final CropInfoDefinition Potatoes;
+  static final CropInfoDefinition Spinach;
+  static final CropInfoDefinition Squash;
+  static final CropInfoDefinition Turnips;
 
   static {
-    Artichokes = new CropInfoDefinition(ArtichokeCropOrb.ORB_ID, new Artichokes(), AbstractCard.CardRarity.COMMON, 2) {
+    Artichokes = new CropInfoDefinition(
+        ArtichokeCropOrb.ORB_ID,
+        new Artichokes(),
+        AbstractCard.CardRarity.COMMON,
+        2) {
       public AbstractCrop getCrop() { return new ArtichokeCrop(); }
     };
 
-    Asparagus = new CropInfoDefinition(AsparagusCropOrb.ORB_ID, new Asparagus(), AbstractCard.CardRarity.UNCOMMON, 3) {
+    Asparagus = new CropInfoDefinition(
+        AsparagusCropOrb.ORB_ID,
+        new Asparagus(),
+        AbstractCard.CardRarity.UNCOMMON,
+        3) {
       public AbstractCrop getCrop() { return new AsparagusCrop(); }
     };
 
-    Chilis = new CropInfoDefinition(ChiliCropOrb.ORB_ID, new Chilis(), AbstractCard.CardRarity.UNCOMMON, 5) {
-      public AbstractCrop getCrop() { return new AsparagusCrop(); }
+    Chilis = new CropInfoDefinition(
+        ChiliCropOrb.ORB_ID,
+        new Chilis(), AbstractCard.CardRarity.UNCOMMON,
+        5) {
+      public AbstractCrop getCrop() { return new ChilisCrop(); }
     };
 
-    Corn = new CropInfoDefinition(CornCropOrb.ORB_ID, new Corn(), AbstractCard.CardRarity.COMMON, 2) {
+    Corn = new CropInfoDefinition(
+        CornCropOrb.ORB_ID,
+        new Corn(),
+        AbstractCard.CardRarity.COMMON,
+        2) {
       public AbstractCrop getCrop() { return new CornCrop(); }
     };
 
-    Mushrooms = new CropInfoDefinition(MushroomCropOrb.ORB_ID, new Mushrooms(), AbstractCard.CardRarity.RARE, 2) {
+    Mushrooms = new CropInfoDefinition(
+        MushroomCropOrb.ORB_ID,
+        new Mushrooms(),
+        AbstractCard.CardRarity.RARE,
+        2) {
       public AbstractCrop getCrop() { return new MushroomCrop(); }
     };
 
-    Onions = new CropInfoDefinition(PotatoCropOrb.ORB_ID, new Potatoes(), AbstractCard.CardRarity.BASIC, 5) {
+    Onions = new CropInfoDefinition(
+        PotatoCropOrb.ORB_ID,
+        new Potatoes(),
+        AbstractCard.CardRarity.BASIC,
+        5) {
       public AbstractCrop getCrop() { return new PotatoCrop(); }
     };
 
-    Potatoes = new CropInfoDefinition(OnionCropOrb.ORB_ID, new Onions(), AbstractCard.CardRarity.BASIC, 5) {
+    Potatoes = new CropInfoDefinition(
+        OnionCropOrb.ORB_ID,
+        new Onions(),
+        AbstractCard.CardRarity.BASIC,
+        5) {
       public AbstractCrop getCrop() { return new OnionCrop(); }
     };
 
-    Spinach = new CropInfoDefinition(SquashCropOrb.ORB_ID, new Squash(), AbstractCard.CardRarity.BASIC, 5) {
+    Spinach = new CropInfoDefinition(
+        SquashCropOrb.ORB_ID,
+        new Squash(),
+        AbstractCard.CardRarity.BASIC,
+        5) {
       public AbstractCrop getCrop() { return new SquashCrop(); }
     };
 
-    Squash = new CropInfoDefinition(SpinachCropOrb.ORB_ID, new Spinach(), AbstractCard.CardRarity.UNCOMMON, 2) {
+    Squash = new CropInfoDefinition(
+        SpinachCropOrb.ORB_ID,
+        new Spinach(),
+        AbstractCard.CardRarity.UNCOMMON,
+        2) {
       public AbstractCrop getCrop() { return new SpinachCrop(); }
     };
 
-    Turnips = new CropInfoDefinition(TurnipCropOrb.ORB_ID, new Turnips(), AbstractCard.CardRarity.COMMON, 5) {
+    Turnips = new CropInfoDefinition(
+        TurnipCropOrb.ORB_ID,
+        new Turnips(),
+        AbstractCard.CardRarity.COMMON,
+        5) {
       public AbstractCrop getCrop() { return new TurnipCrop(); }
     };
   }

1bf0dc9f148ddabfe50010e86af4a846b15552ff
Justin Woolley <19597672+jwoolley@users.noreply.github.com>
2019-08-29 15:37:43 -0700

    moonshine: replace Confusion with Intoxicated

--- src/main/java/thesimpleton/powers/IntoxicatedPower.java
+++ src/main/java/thesimpleton/powers/IntoxicatedPower.java
@@ -1,6 +1,7 @@
 package thesimpleton.powers;
 
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -8,17 +9,17 @@ import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.ConfusionPower;
 import com.megacrit.cardcrawl.powers.SlowPower;
 
-public class SoberUpPower extends AbstractTheSimpletonPower {
+public class IntoxicatedPower extends AbstractTheSimpletonPower {
 
-  public static final String POWER_ID = "TheSimpletonMod:SoberUpPower";
+  public static final String POWER_ID = "TheSimpletonMod:IntoxicatedPower";
   private static final PowerStrings powerStrings;
   public static final String NAME;
   public static final String[] DESCRIPTIONS;
 
-  public static PowerType POWER_TYPE = PowerType.BUFF;
-  public static final String IMG = "soberup.png";
+  public static PowerType POWER_TYPE = PowerType.DEBUFF;
+  public static final String IMG = "intoxicated.png";
 
-  public SoberUpPower(AbstractCreature owner) {
+  public IntoxicatedPower(AbstractCreature owner) {
     super(IMG);
     this.owner = owner;
     this.amount = -1;
@@ -36,11 +37,14 @@ public class SoberUpPower extends AbstractTheSimpletonPower {
   }
 
   @Override
-  public void atEndOfTurn(boolean isPlayer) {
-    this.flash();
-    AbstractDungeon.actionManager.addToTop(
-        new RemoveSpecificPowerAction(this.owner, this.owner, IntoxicatedPower.POWER_ID));
-    AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(this.owner, this.owner, POWER_ID));
+  public void onCardDraw(AbstractCard card) {
+    if (card.cost >= 0) {
+      int newCost = AbstractDungeon.cardRandomRng.random(3);
+      if (card.cost != newCost) {
+        card.setCostForTurn(newCost);
+        card.isCostModifiedForTurn = true;
+      }
+    }
   }
 
   static {
--- src/main/java/thesimpleton/relics/Moonshine.java
+++ src/main/java/thesimpleton/relics/Moonshine.java
@@ -15,6 +15,7 @@ import com.megacrit.cardcrawl.powers.LoseStrengthPower;
 import com.megacrit.cardcrawl.powers.StrengthPower;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 import thesimpleton.TheSimpletonMod;
+import thesimpleton.powers.IntoxicatedPower;
 import thesimpleton.powers.SoberUpPower;
 
 public class Moonshine extends CustomRelic {
@@ -51,19 +52,16 @@ public class Moonshine extends CustomRelic {
       AbstractDungeon.actionManager.addToBottom(new SFXAction("DRINK_BOTTLE_1"));
       AbstractDungeon.actionManager.addToBottom(new SFXAction("GIBBERISH_ANGRY_1"));
       AbstractDungeon.actionManager.addToBottom(
-          new TalkAction(true, DESCRIPTIONS[7], 1.0F, 2.0F));
+          new TalkAction(true, DESCRIPTIONS[8], 1.0F, 2.0F));
 
       AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(player, player,
           new StrengthPower(player, STRENGTH_AMOUNT), STRENGTH_AMOUNT));
       AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(player, player,
           new LoseStrengthPower(player, STRENGTH_AMOUNT), STRENGTH_AMOUNT));
 
-      final ConfusionPower confusionPower = new ConfusionPower(player);
-      confusionPower.priority = 5;
-
       final boolean wasConfused = player.hasPower(ConfusionPower.POWER_ID);
 
-      AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(player, player, confusionPower));
+      AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(player, player, new IntoxicatedPower(player)));
       if (!wasConfused) {
         AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(player, player, new SoberUpPower(player)));
       }
@@ -89,10 +87,11 @@ public class Moonshine extends CustomRelic {
 
   @Override
   public String getUpdatedDescription() {
-    return this.DESCRIPTIONS[0] + TRIGGER_TURN + this.DESCRIPTIONS[1] + STRENGTH_AMOUNT
-        + (DRAW_AMOUNT == 0 ? this.DESCRIPTIONS[2]
-          : this.DESCRIPTIONS[3]
-            + (DRAW_AMOUNT == 1 ? this.DESCRIPTIONS[4] : this.DESCRIPTIONS[5] + DRAW_AMOUNT + this.DESCRIPTIONS[6]));
+    return DESCRIPTIONS[0] + TRIGGER_TURN + DESCRIPTIONS[1] + STRENGTH_AMOUNT
+        + (DRAW_AMOUNT == 0 ? DESCRIPTIONS[2]
+          : DESCRIPTIONS[3] + (DRAW_AMOUNT == 1 ? DESCRIPTIONS[4]
+            : DESCRIPTIONS[5] + DRAW_AMOUNT + DESCRIPTIONS[6]))
+        + DESCRIPTIONS[7];
   }
 
   @Override

b27acc260db2d70c77176d8bb57feeeb3cf1b148
Justin Woolley <19597672+jwoolley@users.noreply.github.com>
2019-09-27 17:50:42 -0700

    static reward numbers

--- src/main/java/thesimpleton/events/GophersEvent.java
+++ src/main/java/thesimpleton/events/GophersEvent.java
@@ -17,6 +17,10 @@ public class GophersEvent extends CustomSimpletonEvent {
   public static final String ID = TheSimpletonMod.makeID("GophersEvent");
 
   private static final String IMG_PATH = SimpletonEventHelper.getUiPath("gophers1");
+  private static final String IMG_PATH_STEP_2 = SimpletonEventHelper.getUiPath("gophers1");
+  private static final String IMG_PATH_STEP_3 = SimpletonEventHelper.getUiPath("gophers1");
+  private static final String IMG_PATH_BITTEN = SimpletonEventHelper.getUiPath("gophers1");
+
   private static final EventStrings eventStrings;
   private static final String NAME;
   private static final String[] DESCRIPTIONS;
@@ -29,9 +33,11 @@ public class GophersEvent extends CustomSimpletonEvent {
   private EventState state;
 
   private final int[] FAIL_CHANCE_PERCENTAGES = { 25, 33, 50 };
+  private final float[] HP_PERCENTAGE_MULTIPLIERS = { 1.0f, 1.5f, 2.0f };
   private static final int MAX_HP_PER_TIER = 5;
   private static final int GOLD_PER_TIER = 65;
   private static final int GOLD_MAX_EXTRA = 20;
+  private static final int POTIONS_PER_TIER = 2;
 
   private static final float HP_PERCENT_LOST_PER_TIER = 0.11F;
 
@@ -104,21 +110,23 @@ public class GophersEvent extends CustomSimpletonEvent {
      * 3) Gain N random (common?) relics
      * 4) Gain N * X Gold
      * 5) Gain N random upgraded Rare cards
-     * 6) Gain X (potion slots and) random Potions
+     * 6) Gain X random Potions
      */
     final int FAIL_PCT = FAIL_CHANCE_PERCENTAGES[this.rewardCounter - 1];
-    final int HP_LOSS =  Math.max ((int)(HP_PERCENT_LOST_PER_TIER * this.rewardCounter * AbstractDungeon.player.maxHealth), 6);
+    final int HP_LOSS =  Math.max ((int)(HP_PERCENT_LOST_PER_TIER * HP_PERCENTAGE_MULTIPLIERS[this.rewardCounter - 1]
+        * AbstractDungeon.player.maxHealth), 6);
     final String loseDesc = OPTIONS[2] + FAIL_PCT + OPTIONS[3] + OPTIONS[4] + HP_LOSS + OPTIONS[5];
+
+    final int rewardMultiplier = 1; // rewardMultiplier = this.rewardCounter;
+
 
     if (this.rewardCounter == 1) {
       this.imageEventText.setDialogOption(OPTIONS[0] + OPTIONS[6] + OPTIONS[7] + loseDesc);
-      this.imageEventText.setDialogOption(OPTIONS[0] + OPTIONS[9] + (this.rewardCounter + MAX_HP_PER_TIER) + OPTIONS[10] + loseDesc);
+      this.imageEventText.setDialogOption(OPTIONS[0] + OPTIONS[9] + (rewardMultiplier + MAX_HP_PER_TIER) + OPTIONS[10] + loseDesc);
 //      this.imageEventText.setDialogOption(OPTIONS[0] + OPTIONS[11] + OPTIONS[12] + loseDesc);
-      this.imageEventText.setDialogOption(OPTIONS[0] + OPTIONS[14] + (this.rewardCounter + goldBonus) + OPTIONS[15] + loseDesc);
+      this.imageEventText.setDialogOption(OPTIONS[0] + OPTIONS[14] + (rewardMultiplier + goldBonus) + OPTIONS[15] + loseDesc);
       this.imageEventText.setDialogOption(OPTIONS[0] + OPTIONS[16] + OPTIONS[17] + loseDesc);
-      this.imageEventText.setDialogOption(OPTIONS[0] + (!AbstractDungeon.player.hasRelic(RELIC_REWARD.relicId)
-            ? OPTIONS[19] + SimpletonUtil.tagString(RELIC_REWARD.name, "#g") + OPTIONS[20] : "")
-            + OPTIONS[21] + OPTIONS[22] + loseDesc);
+      this.imageEventText.setDialogOption(OPTIONS[0] + OPTIONS[21] + rewardMultiplier * POTIONS_PER_TIER + OPTIONS[23] + loseDesc);
       this.imageEventText.setDialogOption(OPTIONS[25]);
 
     } else {
@@ -134,26 +142,44 @@ public class GophersEvent extends CustomSimpletonEvent {
 //      }
 
       if (!chosenOptions.contains(1) && !disabledOptions.contains(1)) {
-        this.imageEventText.updateDialogOption(1,OPTIONS[0] + OPTIONS[9] + this.rewardCounter + MAX_HP_PER_TIER + OPTIONS[10] + loseDesc);
+        if (rewardMultiplier == 1) {
+          this.imageEventText.updateDialogOption(1, OPTIONS[0] + OPTIONS[6] + OPTIONS[7] + loseDesc);
+        } else {
+          this.imageEventText.updateDialogOption(1, OPTIONS[0] + OPTIONS[9] + rewardMultiplier + MAX_HP_PER_TIER + OPTIONS[10] + loseDesc);
+        }
       }
 
       if (!chosenOptions.contains(2) && !disabledOptions.contains(2)) {
-        this.imageEventText.updateDialogOption(2,OPTIONS[0] + OPTIONS[11] + this.rewardCounter + OPTIONS[13] + loseDesc);
+        if (rewardMultiplier == 1) {
+          this.imageEventText.updateDialogOption(2,OPTIONS[0] + OPTIONS[9] + (rewardMultiplier + MAX_HP_PER_TIER) + OPTIONS[10] + loseDesc);
+        } else {
+          this.imageEventText.updateDialogOption(2, OPTIONS[0] + OPTIONS[11] + rewardMultiplier + OPTIONS[13] + loseDesc);
+        }
       }
 
       if (!chosenOptions.contains(3) && !disabledOptions.contains(3)) {
-        this.imageEventText.updateDialogOption(3,OPTIONS[0] + OPTIONS[14] + this.rewardCounter + goldBonus + OPTIONS[15] + loseDesc);
+        if (rewardMultiplier == 1) {
+          this.imageEventText.updateDialogOption(3,OPTIONS[0] + OPTIONS[14] + (rewardMultiplier + goldBonus) + OPTIONS[15] + loseDesc);
+        } else {
+          this.imageEventText.updateDialogOption(3, OPTIONS[0] + OPTIONS[14] + rewardMultiplier + goldBonus + OPTIONS[15] + loseDesc);
+        }
       }
 
       if (!chosenOptions.contains(4) && !disabledOptions.contains(4)) {
-        this.imageEventText.updateDialogOption(4,OPTIONS[0] + OPTIONS[16] + this.rewardCounter + OPTIONS[18] + loseDesc);
+        if (rewardMultiplier == 1) {
+          this.imageEventText.updateDialogOption(4,OPTIONS[0] + OPTIONS[16] + OPTIONS[17] + loseDesc);
+        } else {
+          this.imageEventText.updateDialogOption(4, OPTIONS[0] + OPTIONS[16] + rewardMultiplier + OPTIONS[18] + loseDesc);
+        }
       }
 
       if (!chosenOptions.contains(5) && !disabledOptions.contains(5)) {
-        this.imageEventText.updateDialogOption(5, OPTIONS[0]
-            + (!AbstractDungeon.player.hasRelic(RELIC_REWARD.relicId)
-            ? OPTIONS[19] + SimpletonUtil.tagString(RELIC_REWARD.name, "#g") + OPTIONS[20] : "")
-            + OPTIONS[21] + this.rewardCounter + OPTIONS[23] + loseDesc);
+        if (POTIONS_PER_TIER * rewardMultiplier == 1) {
+          this.imageEventText.updateDialogOption(5,OPTIONS[0] + OPTIONS[21] + OPTIONS[22] + loseDesc);
+        } else {
+          this.imageEventText.updateDialogOption(5, OPTIONS[0] + OPTIONS[21]
+              + POTIONS_PER_TIER * rewardMultiplier + OPTIONS[23] + loseDesc);
+        }
       }
     }
   }

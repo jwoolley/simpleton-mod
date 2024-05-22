a7269aea25b091197b918aba90c8ae4869d962d9
Justin Woolley <jwoolley@users.noreply.github.com>
2024-05-22 01:34:55 -0700

    remove debug logging

--- src/main/java/thesimpleton/cards/status/IceCube.java
+++ src/main/java/thesimpleton/cards/status/IceCube.java
@@ -2,15 +2,12 @@ package thesimpleton.cards.status;
 
 import basemod.abstracts.CustomCard;
 import com.badlogic.gdx.graphics.Color;
-import com.megacrit.cardcrawl.actions.common.ReduceCostAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import thesimpleton.TheSimpletonMod;
-import thesimpleton.devtools.debugging.DebugLogger;
-
 public class IceCube extends CustomCard {
     public static final String ID = "TheSimpletonMod:IceCube";
     private static final CardStrings cardStrings;
@@ -38,23 +35,17 @@ public class IceCube extends CustomCard {
 
     }
 
-    public static final DebugLogger IceCubeLogger = new DebugLogger("IceCubeLogger");
-
     @Override
     public void atTurnStart() {
-        IceCubeLogger.log("Called atTurnStart(). baseMagicNumber: " + this.baseMagicNumber + ", magicNumber: " + magicNumber);
         this.flash(Color.SKY.cpy());
         if (this.baseMagicNumber > 0) {
             this.magicNumber = this.baseMagicNumber =  this.baseMagicNumber - 1;
-            IceCubeLogger.log("Reduced magicNumber. new baseMagicNumber: " + this.baseMagicNumber + ", new magicNumber: " + magicNumber);
             if (this.baseMagicNumber == 0) {
                 CardCrawlGame.sound.play("ICE_CLINK_1");
                 this.superFlash(Color.SKY.cpy());
                 this.isEthereal = true;
                 this.selfRetain = false;
-                IceCubeLogger.log("Set isEthereal to true. isEthereal: " + this.isEthereal);
             }
-            IceCubeLogger.log("Calling updateDescription()");
 
             this.isMagicNumberModified = true;
             updateDescription();
@@ -62,13 +53,8 @@ public class IceCube extends CustomCard {
     }
 
     private void updateDescription() {
-        IceCubeLogger.log("Called updateDescription(). baseMagicNumber: " + this.baseMagicNumber + ", magicNumber: " + magicNumber);
-
         this.rawDescription = getDescription(this.baseMagicNumber);
-        IceCubeLogger.log("Updated rawDescription: " +  this.rawDescription);
         this.initializeDescription();
-        IceCubeLogger.log("Called initializeDescription(). new description: " +  this.description);
-
     }
 
     private static String getDescription(int remainingTurns) {
